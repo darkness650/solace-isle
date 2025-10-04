24849c25cc51012f528669a72a07f6a9234db64a
darkness650 <148747700+darkness650@users.noreply.github.com>
2025-10-04 20:18:19 +0800

    修改autofill注解

--- src/main/java/com/solaceisle/annotation/AutoFill.java
+++ src/main/java/com/solaceisle/annotation/AutoFill.java
@@ -2,6 +2,13 @@ package com.solaceisle.annotation;
 
 import com.solaceisle.pojo.enumeration.OperatorType;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface AutoFill {
     OperatorType value();
 }
